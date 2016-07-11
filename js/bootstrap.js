368fd0269dd46bad9f879f133feb8b10dbeddd23
emilydegrandpre <ylimedeg@gmail.com>
2016-07-11 11:27:51 -0400

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2362,6 +2362,3 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
